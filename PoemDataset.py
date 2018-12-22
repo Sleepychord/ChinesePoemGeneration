1a518f62cc8c08d9a51ebcfb946e9d9852ef9897
topriss <topriss@yahoo.com>
2018-12-22 22:31:13 +0800

    tab改成了空格

--- PoemDataset.py
+++ PoemDataset.py
@@ -8,19 +8,19 @@ import re
 
 
 class PoemDataset(Dataset):
-	def __init__(self, poems, emb):
-		self.poems = sorted(poems, key=len, reverse=True)
-		self.emb = emb
-		self.voc_size, self.emb_dim = self.emb.size()
-		self.emb_dim += self.poems[0].size()[1] - 1
+    def __init__(self, poems, emb):
+        self.poems = sorted(poems, key=len, reverse=True)
+        self.emb = emb
+        self.voc_size, self.emb_dim = self.emb.size()
+        self.emb_dim += self.poems[0].size()[1] - 1
 
-	def __getitem__(self, index):
-		poem = self.poems[index]
-		y = poem[1:, 0].type(torch.long)
-		x1 = poem[:-1, 0].type(torch.long)
-		x1 = torch.nn.functional.embedding(x1, self.emb)
-		x2 = poem[:-1, 1:]
-		return torch.cat((x1, x2), 1), y
+    def __getitem__(self, index):
+        poem = self.poems[index]
+        y = poem[1:, 0].type(torch.long)
+        x1 = poem[:-1, 0].type(torch.long)
+        x1 = torch.nn.functional.embedding(x1, self.emb)
+        x2 = poem[:-1, 1:]
+        return torch.cat((x1, x2), 1), y
 
-	def __len__(self):
-		return len(self.poems)
+    def __len__(self):
+        return len(self.poems)
--- preprocess.py
+++ preprocess.py
@@ -21,81 +21,81 @@ PE_tmp_divider = [float(np.power(PE_const, i / float(dim_PE))) for i in range(di
 
 
 def pos2PE(pos):
-	PE_tmp = pos * np.ones(dim_PE) / PE_tmp_divider
-	PE_tmp[0::2] = np.sin(PE_tmp[0::2])
-	PE_tmp[1::2] = np.cos(PE_tmp[1::2])
-	return PE_tmp
+    PE_tmp = pos * np.ones(dim_PE) / PE_tmp_divider
+    PE_tmp[0::2] = np.sin(PE_tmp[0::2])
+    PE_tmp[1::2] = np.cos(PE_tmp[1::2])
+    return PE_tmp
 
 
 def process_poems(file_name, embedding_file_name):
-	# poems -> list of numbers
-	poems = []
-	poems_PE = []
+    # poems -> list of numbers
+    poems = []
+    poems_PE = []
 
-	with open(file_name, "r", encoding='utf-8', ) as f:
-		for line in f.readlines():
-			# try :
-			line_s = line.strip()
-			title, content = line_s.split(u':')[-2:]
-			content = content.replace(u' ', u'')
-			if u'_' in content or u'(' in content or u'（' in content or u'《' in content or u'[' in content or \
-					start_token in content or end_token in content:
-				continue
-			if len(content) < 5 or len(content) > 79:
-				continue
-			# content = start_token + re.sub('[，。]', '', content) + end_token
-			# if len(re.split("，|。|？|！", content)[0]) != 5:
-			#     continue
-			pos = 0
-			pos_PE = []
-			sep_list = [u'，', u'。', u'？', u'！']
-			for i, word in enumerate(content):
-				if word in sep_list:
-					pos = 0
-				else:
-					pos += 1
-					pos_PE.append(pos2PE(pos))
-			for sep in [u'，', u'。', u'？', u'！']:
-				content = content.replace(sep, u'')
+    with open(file_name, "r", encoding='utf-8', ) as f:
+        for line in f.readlines():
+            # try :
+            line_s = line.strip()
+            title, content = line_s.split(u':')[-2:]
+            content = content.replace(u' ', u'')
+            if u'_' in content or u'(' in content or u'（' in content or u'《' in content or u'[' in content or \
+                start_token in content or end_token in content:
+                continue
+            if len(content) < 5 or len(content) > 79:
+                continue
+            # content = start_token + re.sub('[，。]', '', content) + end_token
+            # if len(re.split("，|。|？|！", content)[0]) != 5:
+            #     continue
+            pos = 0
+            pos_PE = []
+            sep_list = [u'，', u'。', u'？', u'！']
+            for i, word in enumerate(content):
+                if word in sep_list:
+                    pos = 0
+                else:
+                    pos += 1
+                    pos_PE.append(pos2PE(pos))
+            for sep in [u'，', u'。', u'？', u'！']:
+                content = content.replace(sep, u'')
 
-			poems_PE.append(np.array(pos_PE))
-			# poems_pos.append(pos_poem)
-			poems.append(content)
-	# except ValueError as e:
-	# 	pass
+            poems_PE.append(np.array(pos_PE))
+            # poems_pos.append(pos_poem)
+            poems.append(content)
+    # except ValueError as e:
+    # 	pass
 
-	words = list(set([word for poem in poems for word in poem]))
+    words = list(set([word for poem in poems for word in poem]))
 
-	print('Reading embedding...')
-	with open(embedding_file_name, 'r') as f:
-		n, m = map(int, f.readline().split())
-		emb_dict = defaultdict(lambda: np.random.normal(0, 1, size=(m,)))
-		for line in f.readlines():
-			emb = line.split()
-			key = emb.pop(0)
-			emb_dict[key] = np.array(emb, dtype=float)
+    print('Reading embedding...')
+    with open(embedding_file_name, 'r') as f:
+        n, m = map(int, f.readline().split())
+        emb_dict = defaultdict(lambda: np.random.normal(0, 1, size=(m,)))
+        for line in f.readlines():
+            emb = line.split()
+            key = emb.pop(0)
+            emb_dict[key] = np.array(emb, dtype=float)
 
-	voc_size = len(words)
-	emb_dim = m
+    voc_size = len(words)
+    emb_dim = m
 
-	emb = torch.zeros((voc_size, emb_dim), requires_grad=False)
-	word2int = {}
-	for i, word in enumerate(words):
-		emb[i] = torch.tensor(emb_dict[word])
-		word2int[word] = i
+    emb = torch.zeros((voc_size, emb_dim), requires_grad=False)
+    word2int = {}
+    for i, word in enumerate(words):
+        emb[i] = torch.tensor(emb_dict[word])
+        word2int[word] = i
 
-	poems_int = []
-	for i in range(len(poems)):
-		poem_int = torch.tensor([word2int[w] for w in poems[i]], dtype=torch.float).unsqueeze(1)
-		# poem_pos = torch.tensor(poems_pos[i], dtype=torch.long)
-		poem_PE = torch.tensor(poems_PE[i], dtype=torch.float)
-		poems_int.append(torch.cat((poem_int, poem_PE), 1))
+    poems_int = []
+    for i in range(len(poems)):
+        poem_int = torch.tensor([word2int[w] for w in poems[i]], dtype=torch.float).unsqueeze(1)
+        # poem_pos = torch.tensor(poems_pos[i], dtype=torch.long)
+        poem_PE = torch.tensor(poems_PE[i], dtype=torch.float)
+        poems_int.append(torch.cat((poem_int, poem_PE), 1))
 
-	return PoemDataset(poems_int, emb), words, word2int
+    return PoemDataset(poems_int, emb), words, word2int
 
 
 if __name__ == "__main__":
-	dataset, emb, words, _ = process_poems('./data/poems.txt', './data/sgns.sikuquanshu.word')
-	import pdb;
+    dataset, emb, words, _ = process_poems('./data/poems.txt', './data/sgns.sikuquanshu.word')
+    import pdb;
 
-	pdb.set_trace()
+    pdb.set_trace()
